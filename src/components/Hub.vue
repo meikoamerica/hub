6f5b149337d7d54774eb308a188e0ed83768c7fc
Kennith Leung <kleung@meikoamerica.com>
2021-01-13 08:38:01 -0800

    feat: add hub component

---
+++ src/components/Hub.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>Hub</h3>
+    <p>
+      Meiko Staff: You are <b class="warning">not</b> on VPN if you are seeing
+      this page.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.warning {
+  color: crimson;
+}
+</style>
